237b4c8bd3563de03bc1a66fbd786119f6d4227f
K <168924521+Kai-Ofc@users.noreply.github.com>
2025-06-25 00:09:28 -0300

    Painel de configuração

--- Fantasmalém/Assets/Scripts/Audio/AudioController.cs
+++ Fantasmalém/Assets/Scripts/Audio/AudioController.cs
@@ -9,17 +9,6 @@ public class AudioController : MonoBehaviour
     public AudioSource audioSource;
 
     public Slider volumeSlider, effectSlider;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void PlayAudio(int indice) 
     { 
